f4c44e5b70d747b2dfe29c09bea5fb11575aa9e4
Destroxxv <onaje82@gmail.com>
2026-06-07 09:21:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"status": "Diamond Alpha Statcast Worker Running"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
