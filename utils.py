4350c00082b851de0604c5196e01315ade5e013a
Destroxxv <onaje82@gmail.com>
2026-06-07 09:23:10 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
+import hmac
+import hashlib
+import json
+from config import STATCAST_WEBHOOK_SECRET
 
+def sign_payload(payload: dict) -> str:
+    message = json.dumps(payload, sort_keys=True).encode()
+    secret = STATCAST_WEBHOOK_SECRET.encode()
+
+    return hmac.new(secret, message, hashlib.sha256).hexdigest()
